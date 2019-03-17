1c74bcc1bf2e1df8ebfb0d6cbe120d4bd6425f02
Atsushi Kuno <franche1984@gmail.com>
2019-03-18 00:20:26 +0900

    Added some code for AtCoder Beginner Contest

--- ABC/085/testStdFind.cpp
+++
@@ -1,1 +0,0 @@
-#include <vector>#include <iostream>#include <algorithm>int main(){    std::string str = "abcdefg";    std::string write = str.substr(0, 10);    std::cout << "substr: " << write << std::endl;     return 0;}
