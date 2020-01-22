c695a1b1cf8d8c935a51f8a0bb26d5a9d66d26ca
Atsushi Kuno <franche1984@gmail.com>
2020-01-22 22:07:54 +0900

    added some codes

--- ABC/c/abc111_c.cpp
+++ ABC/c/abc111_c.cpp
@@ -1,1 +1,39 @@
-#include <iostream>#include <vector>#include <cmath>const int NUM_ELEM = 1e5 + 1;int main(){    int n;    std::cin >> n;    std::vector<std::vector<long long>> count(2, std::vector<long long>(NUM_ELEM, 0));    for(int i = 0; i < n; ++i){        int val;        std::cin >> val;        ++count.at(i % 2).at(val);    }    auto itrA = std::max_element(count.at(0).begin(), count.at(0).end());    auto itrB = std::max_element(count.at(1).begin(), count.at(1).end());        long long vari = *itrA + *itrB;        int distA = std::distance(count.at(0).begin(), itrA);    int distB = std::distance(count.at(1).begin(), itrB);    std::sort(count.at(0).begin(), count.at(0).end());    std::sort(count.at(1).begin(), count.at(1).end());        if(distA == distB){        long long a = count.at(0).at(NUM_ELEM - 1) + count.at(1).at(NUM_ELEM - 2);        long long b = count.at(1).at(NUM_ELEM - 1) + count.at(0).at(NUM_ELEM - 2);        vari = std::min(a, b);    }    std::cout << n - vari << std::endl;    return 0;}
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+const int NUM_ELEM = 1e5 + 1;
+
+int main(){
+    int n;
+    std::cin >> n;
+
+    std::vector<std::vector<long long>> count(2, std::vector<long long>(NUM_ELEM, 0));
+    for(int i = 0; i < n; ++i){
+        int val;
+        std::cin >> val;
+
+        ++count.at(i % 2).at(val);
+    }
+
+    auto itrA = std::max_element(count.at(0).begin(), count.at(0).end());
+    auto itrB = std::max_element(count.at(1).begin(), count.at(1).end());
+    
+    int distA = std::distance(count.at(0).begin(), itrA);
+    int distB = std::distance(count.at(1).begin(), itrB);
+
+    long long vari = *itrA + *itrB;
+    
+    std::sort(count.at(0).begin(), count.at(0).end());
+    std::sort(count.at(1).begin(), count.at(1).end());
+    
+    if(distA == distB){
+        long long a = count.at(0).at(NUM_ELEM - 1) + count.at(1).at(NUM_ELEM - 2);
+        long long b = count.at(1).at(NUM_ELEM - 1) + count.at(0).at(NUM_ELEM - 2);
+        vari = std::max(a, b);
+    }
+
+    std::cout << n - vari << std::endl;
+
+    return 0;
+}
--- ABC/c/abc128_c.cpp
+++ ABC/c/abc128_c.cpp
@@ -1,1 +1,54 @@
-#include <iostream>#include <vector>#include <bitset>#include <cmath>int main(){    //---------------    // input section    //---------------    int n, m;    std::cin >> n >> m;    std::vector<std::string> vlvs;    for(int i = 0; i < m; ++i){        int k;        std::cin >> k;        std::string str(10, '0');        for(int j = 0; j < k; ++j){            int val;std::cin >> val;            ++str.at(10 - val);        }        vlvs.push_back(str);    }    std::vector<int> p(m);    for(int i = 0; i < m; ++i){        std::cin >> p.at(i);    }    //--------------------    // processing section    //--------------------    int count = 0;    for(int i = 0; i < std::pow(2, n); ++i){        bool flg = true;        std::bitset<10> numBt(i);        for(int j = 0; j < m; ++j){            std::bitset<10> refBt(vlvs.at(j));            refBt &= numBt;            if(refBt.none() || refBt.count() % 2 != p.at(j)){                flg = false;//                break;            }        }                if(flg){            ++count;        }    }    std::cout << count << std::endl;    return 0;}
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <cmath>
+
+int main(){
+    //---------------
+    // input section
+    //---------------
+    int n, m;
+    std::cin >> n >> m;
+
+    std::vector<std::string> vlvs;
+    for(int i = 0; i < m; ++i){
+        int k;
+        std::cin >> k;
+        std::string str(10, '0');
+        for(int j = 0; j < k; ++j){
+            int val;std::cin >> val;
+            ++str.at(10 - val);
+        }
+        vlvs.push_back(str);
+    }
+
+    std::vector<int> p(m);
+    for(int i = 0; i < m; ++i){
+        std::cin >> p.at(i);
+    }
+
+    //--------------------
+    // processing section
+    //--------------------
+    int count = 0;
+    for(int i = 0; i < std::pow(2, n); ++i){
+        bool flg = true;
+        std::bitset<10> numBt(i);
+        for(int j = 0; j < m; ++j){
+            std::bitset<10> refBt(vlvs.at(j));
+            refBt &= numBt;
+            if(refBt.count() % 2 != p.at(j)){
+                flg = false;
+                break;
+            }
+        }
+        
+        if(flg){
+            ++count;
+        }
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
