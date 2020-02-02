cdd08bf9c568b93399042215ac44d88c10648788
Atsushi Kuno <franche1984@gmail.com>
2020-02-02 22:06:56 +0900

    added some codes

---
+++ ABC/c/abc033_c.cpp
@@ -0,0 +1,1 @@
+#include <iostream>int main(){    std::string s;    std::cin >> s;    int sz = s.size();    long long left = s.at(0) - 48, right = 0;    for(int i = 0; i < sz; ++i){            }}
