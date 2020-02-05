b0cfeb468972dd89832f06e521dd6db77663cd3e
Atsushi Kuno <franche1984@gmail.com>
2020-02-05 23:57:03 +0900

    added some codes

--- ABC/c/abc050_c.cpp
+++ ABC/c/abc050_c.cpp
@@ -1,1 +1,50 @@
-#include <iostream>#include <cmath>#include <vector>#include <algorithm>int main(){    int n;    std::cin >> n;    std::vector<int> vec(n);    for(int i = 0; i < n; ++i){        std::cin >> vec.at(i);    }    int count = 0;    int key = vec.size() - 1;    bool flg = true;    while(key >= 0){        if(0 == key){            if(1 != std::count(vec.begin(), vec.end(), key)){                flg = false;                break;            }        }else if(2 != std::count(vec.begin(), vec.end(), key)){            flg = false;            break;        }        key -= 2;    }    if(flg){        std::cout << std::pow(2, vec.size() / 2) << std::endl;    }else{        std::cout << 0 << std::endl;    }    return 0;}
+#include <iostream>
+#include <map>
+
+#define INF 1000000007;
+
+int main(){
+    int n;
+    std::cin >> n;
+
+    std::map<int, int> mp;
+    for(int i = 0; i < n; ++i){
+        int val;
+        std::cin >> val;
+
+        auto itr = mp.find(val);
+        if(mp.end() == itr){
+            mp.insert(std::make_pair(val, 1));
+        }else{
+            ++itr->second;
+        }
+    }
+
+    bool flg = true;
+    auto itr = mp.begin();
+    int ref = 1 - n % 2;
+    while(flg && mp.end() != itr){
+        if(mp.begin() == itr){
+            flg = ref == itr->first && ref + 1 == itr->second;
+        }else{
+            flg = ref == itr->first && 2 == itr->second;
+        }
+        ref += 2;
+        ++itr;
+    }
+
+
+    long long ans = 1;
+    if(flg){
+        for(int i = 0; i < n / 2; ++i){
+            ans *= 2;
+            ans %= INF;
+        }
+    }else{
+        ans = 0;
+    }
+
+    std::cout << ans << std::endl;
+
+    return 0;
+}
