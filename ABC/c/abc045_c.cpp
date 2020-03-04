f4d7965c62ef4f06265cf66ee185455944b12659
Atsushi Kuno <franche1984@gmail.com>
2020-03-04 23:41:47 +0900

    added a code

--- ABC/c/abc045_c.cpp
+++ ABC/c/abc045_c.cpp
@@ -1,1 +1,25 @@
-#include <iostream>#include <cstdlib>int main(){    std::string s;    std::cin >> s;    long long n = s.size(), ans = std::atoi(s.c_str());    for(int bits = 1; bits < (1 << n - 1); ++bits){        int i = 0, done = 0;        for(int j = 0; j < n - 1; ++j){            if(bits & (1 << j)){                ans += std::atoll(s.substr(n - 1 - j, j + 1 - i).c_str());                i = j + 1 - i;                done += i;            }        }                 ans += std::atoll(s.substr(0, n - done).c_str());    }    std::cout << ans << std::endl;    return 0;}
+#include <iostream>
+#include <cstdlib>
+
+int main(){
+    std::string s;
+    std::cin >> s;
+
+    long long n = s.size(), ans = 0;
+    for(int bits = 0; bits < (1 << n - 1); ++bits){
+        int i = n;
+            for(int j = 0; j < n - 1; ++j){
+            if(bits & (1 << j)){
+                int index = n - 1 - j;
+                ans += std::atoll(s.substr(index, i - index).c_str());
+                i = index;
+            }
+        } 
+        
+        ans += std::atoll(s.substr(0, i).c_str());
+    }
+
+    std::cout << ans << std::endl;
+
+    return 0;
+}
