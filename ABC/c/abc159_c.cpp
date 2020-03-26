af85398f7b86609d8ecf30c72c655ba0479f22e2
Atsushi Kuno <franche1984@gmail.com>
2020-03-26 23:54:28 +0900

    added some cods

--- ABC/c/abc159_c.cpp
+++ ABC/c/abc159_c.cpp
@@ -1,23 +1,13 @@
-#include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 int main(){
     long long l;
     std::cin >> l;
     
-    l *= 1e3;
-
-    long long ans = 0;
-    for(long long a = 0; a < std::sqrt(l); ++a){
-        for(long long b = 0; b < std::sqrt(l) - a; ++b){
-            ans = std::max(ans, a * b * (l - a - b));
-        }
-    }
-
-    std::cout << std::setprecision(7) << std::fixed << ans / 1e9 << std::endl;
+    std::cout << std::setprecision(7) << std::fixed << std::pow(l, 3) / 27 << std::endl;
 
     return 0;
 }
